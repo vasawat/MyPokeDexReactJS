cda0bce945c463ae099318d23cdf5dbbccf188b1
Mr.Boss <vasawat77@gmail.com>
2023-08-30 22:26:16 +0700

    complete2 pokedex

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function footer() {
   return (
     <div className='footer'>
-      <div><p className='text-white'>My Github  <a href="">https://www.youtube.com/watch?v=3vClN4RBIOo</a></p></div>
+      <div><p className='text-white'>My Github  <a href="https://github.com/vasawat">https://github.com/vasawat</a></p></div>
     </div>
   )
 }
